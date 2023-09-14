6a06cf704c4c04d855e04d392b341018d97b398a
Muhammad Shandy Dailami <114679625+ShandyDailami@users.noreply.github.com>
2023-09-14 10:13:49 +0800

    Add files via upload

---
+++ PraktikumPemrogramanII/src/PRAKTIKUM1/PRAK001_2210817210013_MuhammadShandyDailami.java
@@ -0,0 +1,9 @@
+package PRAKTIKUM1;
+
+public class PRAK001_2210817210013_MuhammadShandyDailami {
+    public static void main(String[] args) {
+        System.out.println("Selamat Pagi, Muhammad Shandy Dailami");
+        System.out.println("Selamat Siang, Muhammad Shandy Dailami");
+        System.out.println("Selamat Malam, Muhammad Shandy Dailami");
+    }
+}
---
+++ PraktikumPemrogramanII/src/PRAKTIKUM1/PRAK002_2210817210013_MuhammadShandyDailami.java
@@ -0,0 +1,12 @@
+package PRAKTIKUM1;
+
+public class PRAK002_2210817210013_MuhammadShandyDailami {
+    public static void main(String[] args) {
+        System.out.println("##############################");
+        System.out.println("#                            #");
+        System.out.println("#   Muhammad Shandy Dailami  #");
+        System.out.println("#        2210817210013       #");
+        System.out.println("#                            #");
+        System.out.println("##############################");
+    }
+}

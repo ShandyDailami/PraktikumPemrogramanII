380ddb4c24e661fae860c07408c0f3a9b8f10fd5
Muhammad Shandy Dailami <114679625+ShandyDailami@users.noreply.github.com>
2023-09-18 17:21:52 +0800

    Add files via upload

---
+++ PraktikumPemrogramanII/src/PRAKTIKUM2/PRAK101_2210817210013_MuhammadShandyDailami.java
@@ -0,0 +1,7 @@
+package PRAKTIKUM2;
+
+public class PRAK101_2210817210013_MuhammadShandyDailami {
+    public static void main(String[] args) {
+        System.out.println("Selamat Pagi, Muhammad Shandy Dailami");
+    }
+}
